2f1851ef6febc90137cabcaf02ee99ee6bba7b87
brohithkr <brohithkr@gmail.com>
2024-04-27 18:21:22 +0530

    working overhauled code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,79 @@
 import { useState, useMemo } from "react";
-import { Square, Circle, Triangle } from "./components/shapes";
+import { Square, Circle, Triangle, Square2 } from "./components/shapes";
 import { ArcherContainer } from "react-archer";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
+const baseRelation = {
+  targetAnchor: "middle",
+  sourceAnchor: "middle",
+  style: { strokeColor: "#3b82f6", strokeWidth: 5, endMarker: false },
+};
 
 function App() {
   var [selectedArr, setSelectedArr] = useState([]);
-  const reset = () => {
-    window.location.reload()
-  }
-  let grid;
-  useMemo(() => {
-    grid = [];
-    for (let i = 1; i < 13; i++) {
-      grid.push(
-        <div className="flex flex-col">
-          <div className="flex flex-row justify-between">
-            <Square id={`s${i}`} arr={selectedArr} setArr={setSelectedArr}  />
-            <div className="size-10"></div>
-            <Triangle
-              id={`t${i}`} arr={selectedArr} setArr={setSelectedArr}
-            />
-          </div>
-          <div className="flex justify-center">
-            <Circle
-              id={`c${i}`} arr={selectedArr} setArr={setSelectedArr}
-            />
-          </div>
-        </div>
-      );
+  var [gridRelations, setGridRelations] = useState(new Map());
+  let [selectedSet, _] = useState(new Set([]));
+  let handleShapeClick = (id) => {
+    var currentRelation = gridRelations.get(id);
+    if (!selectedArr.includes(id)) {
+      if (selectedArr.length > 0) {
+        if (!currentRelation) {
+          currentRelation = [];
+        }
+        currentRelation.push({
+          targetId: selectedArr[selectedArr.length - 1],
+          targetAnchor: "middle",
+          sourceAnchor: "middle",
+          style: { strokeColor: "#3b82f6", strokeWidth: 5, endMarker: false },
+        });
+        gridRelations.set(id, currentRelation);
+      }
+      selectedSet.add(id);
+      setSelectedArr([...selectedArr, id]);
+    } else {
+      if (selectedArr[selectedArr.length - 1] === id) {
+        selectedSet.delete(id);
+        const newSelectedArr = selectedArr.slice(0, selectedArr.length);
+        newSelectedArr.pop()
+        gridRelations.delete(id);
+        // setGridRelations(gridRelations);
+        // console.log(selectedArr);
+        // setGridRelations(gridRelations);
+        setSelectedArr(newSelectedArr);
+        return;
+      }
     }
-  });
+  };
+
+  const reset = () => {
+    window.location.reload();
+  };
+
+  let grid;
+  // useMemo(() => {
+  grid = [];
+  for (let i = 1; i < 13; i++) {
+    let squareId = `ss${i}`;
+    grid.push(
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <Square
+            id={squareId}
+            isSelected={selectedSet.has(squareId)}
+            onClick={handleShapeClick}
+            color="#3b82f6"
+            relations={gridRelations.get(squareId)}
+          />
+          <div className="size-10"></div>
+          <Triangle id={`t${i}`} arr={selectedArr} setArr={setSelectedArr} />
+        </div>
+        <div className="flex justify-center">
+          <Circle id={`c${i}`} arr={selectedArr} setArr={setSelectedArr} />
+        </div>
+      </div>
+    );
+  }
+  // });
 
   return (
     <div className="flex flex-row">
@@ -42,7 +84,9 @@ function App() {
           </div>
         </ArcherContainer>
       </div>
-      <button onClick={reset} className="w-auto h-9">Reset</button>
+      <button onClick={reset} className="w-auto h-9">
+        Reset
+      </button>
     </div>
   );
 }
--- src/components/shapes.jsx
+++ src/components/shapes.jsx
@@ -3,123 +3,112 @@ import { useState } from "react";
 import { ArcherElement } from "react-archer";
 
 const baseRelation = {
-  targetAnchor: 'middle',
-  sourceAnchor: 'middle',
-  style: { strokeColor: "#3b82f6", strokeWidth: 5, endMarker: false  },
+  targetAnchor: "middle",
+  sourceAnchor: "middle",
+  style: { strokeColor: "#3b82f6", strokeWidth: 5, endMarker: false },
 };
 
-function handleShapeClick(id, isClicked, setIsClicked, arr, setArr, relations, setRelations){
+function handleShapeClick(
+  id,
+  isClicked,
+  setIsClicked,
+  arr,
+  setArr,
+  relations,
+  setRelations
+) {
   let selected = !isClicked;
-    if (selected) {
-      setIsClicked(selected);
-      if (arr.length > 0) {
-        let relation = { ...baseRelation, targetId: arr[arr.length - 1] };
-        setRelations([...relations, relation]);
-      }
-      arr.push(id);
-      setArr(arr);
-    } else {
-      if (arr[arr.length - 1] == id) {
-        setIsClicked(selected);
-        setRelations([])
-        arr.pop();
-      }
+  if (selected) {
+    setIsClicked(selected);
+    if (arr.length > 0) {
+      let relation = { ...baseRelation, targetId: arr[arr.length - 1] };
+      setRelations([...relations, relation]);
     }
+    arr.push(id);
+    setArr(arr);
+  } else {
+    if (arr[arr.length - 1] == id) {
+      setIsClicked(selected);
+      setRelations([]);
+      arr.pop();
+    }
+  }
 }
 
-const Square = ({ id, arr, setArr }) => {
-  const [isClicked, setIsClicked] = useState(false);
-  let [relations, setRelations] = useState([]);
+/**
+ *
+ * @param {Object} props
+ * @param {String} props.id
+ * @param {boolean} props.isSelected
+ * @param {Array<Object>} props.relations
+ * @param {(id: String) => void} props.onClick
+ * @param {string} props.color
+ */
+function Square({ id, isSelected, relations, onClick, color }) {
   const handleClick = () => {
-    let selected = !isClicked;
-    if (selected) {
-      setIsClicked(selected);
-      if (arr.length > 0) {
-        let relation = { ...baseRelation, targetId: arr[arr.length - 1] };
-        setRelations([...relations, relation]);
-      }
-      arr.push(id);
-      setArr(arr);
-    } else {
-      if (arr[arr.length - 1] == id) {
-        setIsClicked(selected);
-        setRelations([])
-        arr.pop();
-      }
-    }
-
+    onClick(id);
   };
+
   return (
     <ArcherElement id={id} relations={relations}>
       <div className="p-2">
         <div
-          onClick={()=>{
-            handleShapeClick(id,isClicked,setIsClicked,arr,setArr,relations,setRelations)
-          }}
+          onClick={handleClick}
           className={`w-10 h-10 flex items-center justify-center  ${
-            isClicked ? "bg-blue-500" : "bg-slate-300"
+            isSelected ? `bg-blue-500` : "bg-slate-300"
           }
-          ${isClicked ? "" : "hover:bg-slate-400"}`}
+          ${isSelected ? "" : "hover:bg-slate-400"}`}
         >
           <div className="-z-10 size-2"></div>
         </div>
       </div>
     </ArcherElement>
   );
-};
+}
 
-const Circle = ({ id, arr, setArr }) => {
-  const [isClicked, setIsClicked] = useState(false);
-  let [relations, setRelations] = useState([]);
+
+const Circle = ({id, isSelected, relations, onClick, color }) => {
 
   const handleClick = () => {
-    setIsClicked(!isClicked);
+    onClick(id);
   };
   return (
     <ArcherElement id={id} relations={relations}>
-    <div className="">
-      <div
-        onClick={()=>{
-          handleShapeClick(id,isClicked,setIsClicked,arr,setArr,relations,setRelations)
-        }}
-        className={`p-3 w-10 items-center justify-center h-10 rounded-full  ${
-          isClicked ? "" : "hover:bg-slate-400"
-        } ${isClicked ? "bg-blue-500" : "bg-slate-300"}`}
-      ></div>
-    </div>
+      <div className="">
+        <div
+          onClick={handleClick}
+          className={`p-3 w-10 items-center justify-center h-10 rounded-full  ${
+            isSelected ? "" : "hover:bg-slate-400"
+          } ${isSelected ? "bg-blue-500" : "bg-slate-300"}`}
+        ></div>
+      </div>
     </ArcherElement>
   );
 };
 
-const Triangle = ({ id, arr, setArr }) => {
-  let height = (Math.sqrt(3) / 2) * 42;
-  let h = height.toString().substring(0, 4);
-
-  const [isClicked, setIsClicked] = useState(false);
-  let [relations, setRelations] = useState([]);
+const Triangle = ({ id, isSelected, relations, onClick, color  }) => {
+  // let height = (Math.sqrt(3) / 2) * 42;
+  // let h = height.toString().substring(0, 4);
 
   const handleClick = () => {
-    setIsClicked(!isClicked);
-    onClick();
+    onClick(id);
   };
 
   return (
     <div className="p-2 items-center justify-center">
       <div
-        onClick={()=>{
-          handleShapeClick(id,isClicked,setIsClicked,arr,setArr,relations,setRelations)
-        }}
+        onClick={handleClick}
         className={`size-0
         border-l-[21px] border-l-transparent
         border-r-[21px] border-r-transparent
         border-b-[34.6px] ${
-          isClicked ? "border-b-blue-500" : "border-b-slate-300"
+          isSelected ? "border-b-blue-500" : "border-b-slate-300"
         }
-        ${isClicked ? "" : "hover:border-b-slate-400"}
+        ${isSelected ? "" : "hover:border-b-slate-400"}
         `}
       >
         <ArcherElement id={id} relations={relations}>
-        <div className="-z-10 size-2 relative left-[0.3px] top-4  "></div>
+          <div className="-z-10 size-2 relative left-[0.3px] top-4  "></div>
         </ArcherElement>
       </div>
     </div>
